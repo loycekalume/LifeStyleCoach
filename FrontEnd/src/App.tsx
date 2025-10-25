bace2516f3aba86a9195ff3eaf031a2ea0e18e94
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-10-25 10:14:49 +0300

    added client and login routes

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/signUp";   
 import Login from "./pages/Login";    
 import Home from "./pages/home";
+import ClientDashboard from "./pages/Client"
 
 const App: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const App: React.FC = () => {
         {/* Auth pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/client" element={<ClientDashboard/>}/>
 
       </Routes>
     </Router>
