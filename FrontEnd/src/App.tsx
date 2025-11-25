ab43c2d27e86b97551771694c1f1f94e8c9eb768
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-11-25 09:39:20 +0300

    updated styling

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,24 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./pages/signUp";   
-import Login from "./pages/Login";    
+import Signup from "./pages/signUp";
+import Login from "./pages/Login";
 import Home from "./pages/home";
 import ClientDashboard from "./pages/Client"
 import InstructorDashboard from "./pages/instructor"
+import Workouts from "./components/instructor/workouts";
+import SessionsPage from "./components/instructor/allSessions";
+import ProfileLoader from "./routes/profileRoutes";
+import DieticianDashboard from "./pages/dietician";
+import AdminDashboard from "./pages/admin";
+import DieticianList from "./components/admin/dieticianLists";
+import InstructorList from "./components/admin/instructorList";
+import ClientList from "./components/admin/clientsList";
+import { ToastContainer } from "react-toastify";
+import ClientsPage from "./components/ClientsView";
 
 const App: React.FC = () => {
   return (
     <Router>
+          <ToastContainer position="top-right" autoClose={2500} />
       <Routes>
-        
+
         <Route path="/" element={<Home />} />
 
         {/* Auth pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/client" element={<ClientDashboard/>}/>
-         <Route path="/instructor" element={<InstructorDashboard/>}/>
-
+        <Route path="/client" element={<ClientDashboard />} />
+        <Route path="/instructor" element={<InstructorDashboard />} />
+        <Route path="/dietician" element={<DieticianDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/instructors" element={<InstructorList />} />
+        <Route path="/admin/dieticians" element={<DieticianList />} />
+        <Route path="/admin/clients" element={<ClientList />} />
+         <Route path="clientsView" element={<ClientsPage />} />
+     
+        <Route
+          path="/complete-profile"
+          element={<ProfileLoader />} // This now loads ClientProfileWizard OR InstructorProfileWizard
+        />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/sessions" element={<SessionsPage />} />
       </Routes>
     </Router>
   );
