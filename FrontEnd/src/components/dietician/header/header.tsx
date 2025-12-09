306176409f15b1577d56bcc5d00e8eaa9419b031
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-12-09 08:37:36 +0300

    added schedule modal

--- FrontEnd/src/components/dietician/header/header.tsx
+++ FrontEnd/src/components/dietician/header/header.tsx
@@ -1,23 +1,32 @@
 import Logo from "./logo";
 import ProfileDropdown from "./profileDropdown";
+import { useModal } from "./../../../contexts/modalContext"; 
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="container">
-        <div className="header-content">
+  const { openMealPlanModal, openConsultationModal } = useModal(); 
 
-          <div className="header-left">
+  return (
+    <header className="header1">
+      <div className="container">
+        <div className="header1-content">
+
+          <div className="header1-left">
             <Logo />
             <h1>Dietician Dashboard</h1>
           </div>
 
-          <div className="header-actions">
-            <button className="btn btn-outline1">
+          <div className="header1-actions">
+            <button 
+              className="btn btn-outline1"
+              onClick={openMealPlanModal} 
+            >
               <i className="fas fa-plus"></i> New Meal Plan
             </button>
 
-            <button className="btn btn-outline1">
+            <button 
+              className="btn btn-outline1"
+              onClick={openConsultationModal}
+            >
               <i className="fas fa-calendar-plus"></i> Schedule Consultation
             </button>
 
