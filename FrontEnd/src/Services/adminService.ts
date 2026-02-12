478f2d7707ecee034e6c23f0b847a0695ea131a7
Nalish <sharonnaliaka@gmail.com>
2026-02-12 14:46:18 +0300

    removing url

--- FrontEnd/src/Services/adminService.ts
+++ FrontEnd/src/Services/adminService.ts
@@ -1,7 +1,7 @@
 // src/Services/adminService.ts
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL ;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
